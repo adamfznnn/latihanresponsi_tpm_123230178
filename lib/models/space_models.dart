994f2d8a35785ba6a4f0175b5ef4fa570af4c4d6
unknown <adamfwaskito@gmail.com>
2026-05-21 20:31:41 +0700

    first commit

--- lib/models/space_models.dart
+++ lib/models/space_models.dart
@@ -1,5 +1,3 @@
-// lib/models/space_model.dart
-
 class SpaceResponse {
   final int count;
   final List<SpaceModel> results;
@@ -9,7 +7,6 @@ class SpaceResponse {
     required this.results,
   });
 
-  // Fungsi untuk mem-parsing JSON utama (yang memiliki key 'results')
   factory SpaceResponse.fromJson(Map<String, dynamic> json) {
     return SpaceResponse(
       count: json['count'] ?? 0,
@@ -39,13 +36,12 @@ class SpaceModel {
     required this.publishedAt,
   });
 
-  // Fungsi untuk mem-parsing JSON detail dari masing-masing item
   factory SpaceModel.fromJson(Map<String, dynamic> json) {
     return SpaceModel(
       id: json['id'] ?? 0,
       title: json['title'] ?? 'No Title',
       url: json['url'] ?? '',
-      imageUrl: json['image_url'] ?? 'https://via.placeholder.com/150', // Fallback gambar
+      imageUrl: json['image_url'] ?? 'https://via.placeholder.com/150',
       newsSite: json['news_site'] ?? 'Unknown Site',
       summary: json['summary'] ?? 'No Summary available.',
       publishedAt: json['published_at'] ?? '',
